c1013913e27d09a129ef11b510faa118deab5c42
Dan Dart <git@dandart.co.uk>
2021-07-14 20:04:40 +0100

    netbsd

---
+++ BSD/NetBSD/create-netbsd.sh
@@ -0,0 +1,1 @@
+qemu-img create -f qcow2 netbsd.qcow2 10G
---
+++ BSD/NetBSD/get-netbsd.sh
@@ -0,0 +1,2 @@
+wget -c https://cdn.netbsd.org/pub/NetBSD/NetBSD-9.2/images/NetBSD-9.2-amd64-install.img.gz
+gunzip NetBSD-9.2-amd64-install.img.gz
