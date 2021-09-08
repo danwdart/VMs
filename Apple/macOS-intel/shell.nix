ec22e06985bef84541cb9afd21a394ef0d44ab2e
Dan Dart <git@dandart.co.uk>
2021-09-09 00:17:10 +0100

    Update nixpkgs

--- Apple/macOS-intel/shell.nix
+++ Apple/macOS-intel/shell.nix
@@ -1,4 +1,4 @@
-with import <unstable> {};
+with import  (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
 runCommand "macOS-intel" {
     buildInputs = [
         dmg2img
--- Windows/10/shell.nix
+++ Windows/10/shell.nix
@@ -1,4 +1,4 @@
-with import <unstable> {};
+with import  (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
 runCommand "win10" {
     shellHook = ''
         [ -f OVMF_VARS.fd ] || cp ${OVMF.fd.outPath}/FV/OVMF_CODE.fd .
