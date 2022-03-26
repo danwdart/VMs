d33529a31239580586cc5bc066f5382ec92f26a3
Dan Dart <git@dandart.co.uk>
2022-03-26 03:54:16 +0000

    get win2k

---
+++ Windows/2000/get.sh
@@ -0,0 +1,4 @@
+wget --content-disposition https://winworldpc.com/download/4137770d-e828-11e7-a562-fa163e9022f0/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
+7z x Microsoft Windows 2000 Professional (with SP4) (Select) (ISO).7z
+mv Microsoft Windows 2000 Professional (with SP4) (Select) (ISO)/5.00.2195.6717_x86fre_client-professional_retail_en-us-ZRMPSEL_EN.iso .
+rm -rf Microsoft\ Windows\ 2000\ Professional\ \(with\ SP4\)\ \(Select\)\ \(ISO\)*
