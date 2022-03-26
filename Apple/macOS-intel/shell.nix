4a84bec44bd5ca26e96c97a70910e7d8bc069848
Dan Dart <git@dandart.co.uk>
2022-03-26 03:54:37 +0000

    include libguestfs with appliance for macos intel

--- Apple/macOS-intel/shell.nix
+++ Apple/macOS-intel/shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 runCommand "macOS-intel" {
     buildInputs = [
         dmg2img
+        libguestfs-with-appliance
         python39
         python39Packages.internetarchive
         qemu
