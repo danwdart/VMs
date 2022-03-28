e6399b7c7d12e86a9d5ebf5228c9545d8df4bd5d
Dan Dart <git@dandart.co.uk>
2022-03-28 23:25:03 +0100

    get dos gets tools

--- DOS/MS-DOS/6.22/get-msdos622.sh
+++ DOS/MS-DOS/6.22/get-msdos622.sh
@@ -2,3 +2,13 @@ wget https://cloudflare-ipfs.com/ipfs/QmSeHBGoMxSjCzkaJEpkshkopQKPWhgFT8z1XJC3jg
 7z x Microsoft\ MS-DOS\ 6.22\ Plus\ Enhanced\ Tools\ \(3.5\).7z
 mv Microsoft\ MS-DOS\ 6.22\ Plus\ Enhanced\ Tools\ \(3.5\)/*.img .
 rm Microsoft\ MS-DOS\ 6.22\ Plus\ Enhanced\ Tools\ \(3.5\)* -rf
+wget http://web.archive.org/web/20051222085335/http://www.vmware.com/software/dosidle210.zip
+mkdir dosutils
+cd dosutils
+7z x ../dosidle210.zip
+cd ..
+rm dosidle210.zip
+dd if=/dev/zero of=dosutils.img count=2880
+mkfs.msdos dosutils.img
+mcopy -i dosutils.img dosutils/* ::/
+rm -rf dosutils
