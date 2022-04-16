00351b37159816d5884d58c4ca22a5ca2bc5d1a5
Dan Dart <git@dandart.co.uk>
2022-04-16 04:45:00 +0100

    win98 less mem

--- Windows/98/install-win98se.sh
+++ Windows/98/install-win98se.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -enable-kvm -cpu pentium3 -m 256 -usb -device usb-tablet -device sb16 -nic user,model=ne2k_pci -no-hpet win98se.img -cdrom win98se.iso -fda boot.img -boot a -monitor stdio
+qemu-system-i386 -enable-kvm -cpu pentium3 -m 64 -usb -device usb-tablet -device sb16 -nic user,model=ne2k_pci -no-hpet win98se.img -cdrom win98se.iso -fda boot.img -boot a -monitor stdio
