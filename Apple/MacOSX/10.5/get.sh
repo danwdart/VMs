526ad1a9257a24f3fb5b37765b80c9e5ca231380
Dan Dart <git@dandart.co.uk>
2022-05-22 19:34:07 +0100

    filelist for mac 10.5

--- Apple/MacOSX/10.5/get.sh
+++ Apple/MacOSX/10.5/get.sh
@@ -1,2 +1,1 @@
-aria2c -s16 -j16 -x16 -k1M -c https://updates.cdn-apple.com/2019/cert/041-85213-20191017-1c7ca848-489c-4562-9a6a-2cfe4e04ccb0/MacOSXUpdCombo10.5.8.dmg
-aria2c -s16 -j16 -x16 -k1M -c https://archive.org/download/OsxLeopardInstall/osx%20leopard%20install.iso
+aria2c -s16 -j16 -x16 -k1M -c -i filelist
