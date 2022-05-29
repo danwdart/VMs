e692f153222ea7f763186e425f6856ac33f55f72
Dan Dart <git@dandart.co.uk>
2022-05-29 19:00:58 +0100

    Update get-win11

--- Windows/11/get-win11.sh
+++ Windows/11/get-win11.sh
@@ -1,4 +1,3 @@
-# aria2c -s16 -j16 -x16 -k1M -c "https://software.download.prss.microsoft.com/db/Win11_EnglishInternational_x64v1.iso?t=2ad12ce4-9da2-479e-9734-40c21d789d0e&e=1648218089&h=51d5da781873d9aa277e86e9ac5c18e4af3e7b1c9a6b6c4ccb8119d9b2f3c229" -oWin11_EnglishInternational_x64v1.iso
-xdg-open https://www.microsoft.com/en-gb/software-download/windows11/
+aria2c -s16 -j16 -x16 -k1M -c -oWin11_EnglishInternational_x64v1.iso "https://software.download.prss.microsoft.com/db/Win11_EnglishInternational_x64v1.iso?t=b1aa233c-530a-4f62-8d10-09f193c29adf&e=1653866222&h=ff488c69c047584a731a460ff15cbbf5e72825704c7e43b3fe5305f5db71285f" 
 aria2c -s16 -j16 -x16 -k1M -c https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso
 
