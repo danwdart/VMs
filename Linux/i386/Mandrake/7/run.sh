a7b9809ebaf48aecdf424d31b67ced3d3fa7b1c0
Dan Dart <git@dandart.co.uk>
2022-06-09 01:22:55 +0100

    kvm for mandrake 7

---
+++ Linux/i386/Mandrake/7/run.sh
@@ -0,0 +1,1 @@
+qemu-system-i386 -enable-kvm -cpu pentium3 -m 256 mandrake.img -cdrom CDROM1.iso -nic user,model=ne2k_pci -monitor stdio  -usb -device usb-tablet -device usb-kbd
--- Linux/i386/Mandrake/7/start.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-i386 -cpu pentium3 -m 256 mandrake.img -cdrom CDROM1.iso -nic user,model=ne2k_pci -monitor stdio  -usb -device usb-tablet -device usb-kbd
