ea95fb74f7e4c9a32d126ebe9b829d05fc6ee128
Dan Dart <git@dandart.co.uk>
2022-06-09 01:23:40 +0100

    fix nt4 get script

--- Windows/NT4/get.sh
+++ Windows/NT4/get.sh
@@ -1,4 +1,4 @@
 aria2c -s16 -j16 -x16 -k1M --content-disposition https://winworldpc.com/download/4162c2ba-c3b7-18c3-9a11-c3a4e284a2ef/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 7z x "Microsoft Windows NT 4.0 Workstation (4.00.1381.1.sp1).7z"
 mv "Microsoft Windows NT 4.0 Workstation (4.00.1381.1.sp1)/winnt40wks_sp1_en.iso" .
-rm -rf "Microsoft Windows NT 4.0 Workstation (4.00.1381.1.sp1)*"
+rm -rf Microsoft\ Windows\ NT\ 4.0\ Workstation\ \(4.00.1381.1.sp1\)*
