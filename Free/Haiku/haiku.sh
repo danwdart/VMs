cc3c884885ca1b0b2b00db81ef172a8e2b114155
Dan Dart <git@dandart.co.uk>
2022-08-27 03:56:23 +0100

    haiku fixes

--- Free/Haiku/haiku.sh
+++ Free/Haiku/haiku.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -usb -m 1024 haiku.img -device usb-tablet -device usb-kbd $QEMU_OVMF
+$QEMU_X86_64 -usb -m 1024 haiku.img $QEMU_USB $QEMU_DISP_GL $QEMU_OVMF
--- Free/Haiku/install.sh
+++ Free/Haiku/install.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -usb -m 1024 haiku.img -cdrom haiku-r1beta3-x86_64-anyboot.iso -device usb-tablet -device usb-kbd -boot d $QEMU_OVMF
+$QEMU_X86_64 -usb -m 1024 haiku.img -cdrom haiku-r1beta3-x86_64-anyboot.iso -boot d $QEMU_USB $QEMU_DISP_GL $QEMU_OVMF
