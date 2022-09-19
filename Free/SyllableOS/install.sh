80c14671852fbd0dab99c4e76799b87fad222ec5
Dan Dart <git@dandart.co.uk>
2022-09-19 18:16:06 +0100

    syllable x86_64 -> i386

--- Free/SyllableOS/install.sh
+++ Free/SyllableOS/install.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -usb -m 1024 syllableos.img -cdrom *.iso -boot d $QEMU_USB $QEMU_DISP_GL
+$QEMU_I386 -m 1024 syllableos.img -cdrom *.iso -boot d $QEMU_USB $QEMU_DISP_GL
--- Free/SyllableOS/run.sh
+++ Free/SyllableOS/run.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -usb -m 1024 syllableos.img $QEMU_USB $QEMU_DISP_GL
+$QEMU_I386 -m 1024 syllableos.img $QEMU_USB $QEMU_DISP_GL
