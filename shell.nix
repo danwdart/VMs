8e2d37b15833e19c6af5fb373b977214ff2b9542
Dan Dart <git@dandart.co.uk>
2022-10-10 01:47:30 +0100

    update shell.nix with new envs

--- shell.nix
+++ shell.nix
@@ -9,26 +9,27 @@ runCommand "VMs" {
         export OVMF_VARS=$QEMU_PATH/edk2-i386-vars.fd
         export AAVMF_CODE=$QEMU_PATH/edk2-aarch64-code.fd
         export AAVMF_VARS=$QEMU_PATH/edk2-arm-vars.fd
-        export QEMU_X86="qemu-system-i386 -M q35 -enable-kvm -cpu host -smp cores=2,threads=2"
+        export QEMU_I386="qemu-system-i386 -enable-kvm"
+        export QEMU_X86="qemu-system-i386 -M q35 -enable-kvm -cpu host -smp cores=4,threads=2"
         export QEMU_X86_64="qemu-system-x86_64 -M q35 -enable-kvm -cpu host -smp cores=4,threads=2"
         export QEMU_X86_64_NETBSD="qemu-system-x86_64 -M q35 -enable-kvm -cpu host,sse2=off -smp cores=4,threads=2"
+        export QEMU_RPI3B="qemu-system-aarch64 -M raspi3b -m 1G -cpu cortex-a53 -serial stdio -smp 4 $QEMU_USB -device usb-net"
+        export QEMU_AARCH64="qemu-system-aarch64 -M virt -m 4G -cpu cortex-a72 -serial stdio -smp 4 $QEMU_VIRTIO_PERIPH -device virtio-net $QEMU_AAVMF -device ramfb -boot menu=on -device nec-usb-xhci $QEMU_USB"
+        VMSDIR=$(pwd)
+        export QEMU_RISCV64="qemu-system-riscv64 -M virt -m 2G -cpu rv64 -smp cores=4,threads=2"
         export QEMU_TPM="-tpmdev passthrough,id=tpm0,path=/dev/tpm0 -device tpm-tis,tpmdev=tpm0"
         export QEMU_TPM_EMU="-chardev socket,id=chrtpm,path=tpm0 -tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0"
         export QEMU_USB="-usb -device usb-kbd -device usb-tablet"
         export QEMU_VIRTIO_PERIPH="-device virtio-keyboard -device virtio-tablet"
         export QEMU_DISP_GL="-device virtio-vga-gl,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT -display gtk,gl=on"
-        export QEMU_DISP_STD="-device VGA,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT,xmax=$SCREEN_WIDTH,ymax=$SCREEN_HEIGHT"
-        export QEMU_DISP_QXL="-device qxl-vga,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT"
+        export QEMU_DISP_STD="-device VGA,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT,xmax=$SCREEN_WIDTH,ymax=$SCREEN_HEIGHT,vgamem_mb=128"
+        export QEMU_DISP_QXL="-device qxl-vga,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT,vgamem_mb=128"
+        export QEMU_DISP_VMWARE="-device vmware-svga,vgamem_mb=128"
         export QEMU_OVMF="-drive file=$OVMF_CODE,if=pflash,format=raw,readonly=on -drive file=edk2-i386-vars.fd,if=pflash,format=raw"
         export QEMU_AAVMF="-drive file=$AAVMF_CODE,if=pflash,format=raw,readonly=on -drive file=edk2-arm-vars.fd,if=pflash,format=raw"
         export QEMU_SECURE_OVMF="-drive file=$OVMF_SECURE_CODE,if=pflash,format=raw,readonly=on -drive file=edk2-i386-vars.fd,if=pflash,format=raw"
         export QEMU_BALLOON="-device virtio-balloon-pci,deflate-on-oom=true"
         export QEMU_RNG="-object rng-random,filename=/dev/urandom,id=rng -device virtio-rng-device,rng=rng"
-        export QEMU_I386="qemu-system-i386 -enable-kvm"
-        export QEMU_RPI3B="qemu-system-aarch64 -M raspi3b -m 1G -cpu cortex-a53 -serial stdio -smp 4 $QEMU_USB -device usb-net"
-        export QEMU_AARCH64="qemu-system-aarch64 -M virt -m 4G -cpu cortex-a72 -serial stdio -smp 4 $QEMU_VIRTIO_PERIPH -device virtio-net $QEMU_AAVMF -device ramfb -boot menu=on -device nec-usb-xhci $QEMU_USB"
-        VMSDIR=$(pwd)
-        export QEMU_RISCV64="qemu-system-riscv64 -M virt -m 2G -cpu rv64 -smp cores=4,threads=2"
         find -name "*.sh" -exec sh -c '
             DIRNAME="$(dirname "{}")"
             [ -f "$DIRNAME/edk2-i386-vars.fd" ] || cp --no-preserve=mode,ownership $OVMF_VARS "$DIRNAME"
@@ -42,7 +43,7 @@ runCommand "VMs" {
         cabextract
         chntpw
         wimlib
-        swtpm-tpm2
+        swtpm
         tpm2-tools
         win-virtio
     ];
