f5099d159e977660bebc99951076e50e10bac6c7
Dan Dart <git@dandart.co.uk>
2022-10-11 19:56:42 +0100

    xp ne2k_pci

--- Proprietary/Windows/Legacy/XP/Home/run.sh
+++ Proprietary/Windows/Legacy/XP/Home/run.sh
@@ -1,1 +1,1 @@
-$QEMU_I386 -m 512 $QEMU_USB -device ich9-intel-hda -nic user xphome.img -cdrom *.iso -monitor stdio
+$QEMU_I386 -m 512 $QEMU_USB -device ich9-intel-hda -nic user,model=ne2k_pci xphome.img -cdrom *.iso -monitor stdio
