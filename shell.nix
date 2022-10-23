e0e36a78e6472e7bc0b82a1c68879b42b1f21c3f
Dan Dart <git@dandart.co.uk>
2022-10-23 19:55:10 +0100

    add gvt-g stuff

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ runCommand "VMs" {
     shellHook = ''
         export SCREEN_WIDTH=1920
         export SCREEN_HEIGHT=1080
+        export GVT_UUID=f3391b14-5221-11ed-ba3d-6b25560d08e7
         export QEMU_PATH=${qemu.outPath}/share/qemu/
+        export ROOT_PATH=$(pwd)
         export OVMF_CODE=$QEMU_PATH/edk2-x86_64-code.fd
         export OVMF_SECURE_CODE=$QEMU_PATH/edk2-x86_64-secure-code.fd
         export OVMF_VARS=$QEMU_PATH/edk2-i386-vars.fd
@@ -21,6 +23,8 @@ runCommand "VMs" {
         export QEMU_TPM_EMU="-chardev socket,id=chrtpm,path=tpm0 -tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0"
         export QEMU_USB="-usb -device usb-kbd -device usb-tablet"
         export QEMU_VIRTIO_PERIPH="-device virtio-keyboard -device virtio-tablet"
+        export QEMU_DISP_GVT="-device vfio-pci,sysfsdev=/sys/bus/mdev/devices/$GVT_UUID,display=on,x-igd-opregion=on,driver=vfio-pci-nohotplug,ramfb=on,xres=1920,yres=1080  -display spice-app,gl=on"
+        export QEMU_DISP_GVT_OVMF="-device vfio-pci,sysfsdev=/sys/bus/mdev/devices/$GVT_UUID,display=on,x-igd-opregion=on,driver=vfio-pci-nohotplug,ramfb=on,driver=vfio-pci-nohotplug,romfile=$ROOT_PATH/vbios_gvt_uefi.rom,xres=1920,yres=1080 -display spice-app,gl=on"
         export QEMU_DISP_GL="-device virtio-vga-gl,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT -display spice-app,gl=on"
         export QEMU_DISP_STD="-device VGA,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT,xmax=$SCREEN_WIDTH,ymax=$SCREEN_HEIGHT,vgamem_mb=128 -display spice-app"
         export QEMU_DISP_QXL="-device qxl-vga,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT,vgamem_mb=128 -display spice-app.gl=on"
@@ -30,6 +34,7 @@ runCommand "VMs" {
         export QEMU_SECURE_OVMF="-drive file=$OVMF_SECURE_CODE,if=pflash,format=raw,readonly=on -drive file=edk2-i386-vars.fd,if=pflash,format=raw"
         export QEMU_BALLOON="-device virtio-balloon-pci,deflate-on-oom=true"
         export QEMU_RNG="-object rng-random,filename=/dev/urandom,id=rng -device virtio-rng-device,rng=rng"
+        [ -f "vbios_gvt_uefi.rom" ] || aria2c -s16 -j16 -x16 -k1M --content-disposition -c https://web.archive.org/web/20201020144354/http://120.25.59.132:3000/vbios_gvt_uefi.rom
         find -name "*.sh" -exec sh -c '
             DIRNAME="$(dirname "{}")"
             [ -f "$DIRNAME/edk2-i386-vars.fd" ] || cp --no-preserve=mode,ownership $OVMF_VARS "$DIRNAME"
