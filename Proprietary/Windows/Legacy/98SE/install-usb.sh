3ee5abdec7925d8a87622a8f39fd97108c46e837
Dan Dart <git@dandart.co.uk>
2022-11-27 02:12:33 +0000

    98se: append $@ and add run-no-acpi

--- Proprietary/Windows/Legacy/98SE/install-usb.sh
+++ Proprietary/Windows/Legacy/98SE/install-usb.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -m 512 $QEMU_DISP_STD -device usb-audio -usb -device usb-tablet -nic user,model=ne2k_pci -no-hpet -drive file=win98se.img,if=none,id=hd -device usb-storage,drive=hd -fda boot.img -boot a -monitor stdio
+$QEMU_X86_64 -m 512 $QEMU_DISP_STD -device usb-audio -usb -device usb-tablet -nic user,model=ne2k_pci -no-hpet -drive file=win98se.img,if=none,id=hd -device usb-storage,drive=hd -fda boot.img -boot a -monitor stdio $@
--- Proprietary/Windows/Legacy/98SE/install-virtio.sh
+++ Proprietary/Windows/Legacy/98SE/install-virtio.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64 -m 512 $QEMU_DISP_STD -usb -device usb-tablet -device sb16 -nic user,model=ne2k_pci -no-hpet -drive file=win98se.img,if=virtio -fda boot.img -boot a -monitor stdio
+$QEMU_X86_64 -m 512 $QEMU_DISP_STD -usb -device usb-tablet -device sb16 -nic user,model=ne2k_pci -no-hpet -drive file=win98se.img,if=virtio -fda boot.img -boot a -monitor stdio $@
