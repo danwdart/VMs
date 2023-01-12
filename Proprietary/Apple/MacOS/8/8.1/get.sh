4a0714c8bbccd67a0709f83e3b08c7e06ce0e7d8
Dan Dart <git@dandart.co.uk>
2023-01-12 20:59:08 +0000

    get script for macos 8.1

---
+++ Proprietary/Apple/MacOS/8/8.1/get.sh
@@ -0,0 +1,7 @@
+get_installer
+7z x Apple\ Mac\ OS\ 8.1\ \(ISO\).7z
+rm Apple\ Mac\ OS\ 8.1\ \(ISO\).7z
+mv Apple\ Mac\ OS\ 8.1\ \(ISO\)/MacOS8_1.iso .
+unzip Q800.zip
+rm Q800.zip
+rm -rf Apple\ Mac\ OS\ 8.1\ \(ISO\)/
