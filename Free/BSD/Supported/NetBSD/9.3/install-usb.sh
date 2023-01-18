5bb873d4e93d93e98a5311edc4f25c7587eb2848
Dan Dart <git@dandart.co.uk>
2023-01-18 00:16:12 +0000

    attempt to fix netbsd but not there just yet

---
+++ Free/BSD/Supported/NetBSD/9.3/install-usb.sh
@@ -0,0 +1,6 @@
+# https://gnats.netbsd.org/56361
+# NetBSD dies due to a SSE2 bug.
+$QEMU_X86_64_NETBSD -device qemu-xhci $QEMU_USB \
+    -device usb-storage,drive=hd -drive file=netbsd.img,if=none,id=hd,format=raw \
+    -device usb-storage,drive=installer -drive file=NetBSD-9.3-amd64-install.img,if=none,id=installer,format=raw \
+    $QEMU_DISP_GL -boot menu=on -m 2048 $QEMU_OVMF
--- Free/BSD/Supported/NetBSD/9.3/netbsd.sh
+++
@@ -1,1 +0,0 @@
-$QEMU_X86_64_NETBSD netbsd.img $QEMU_USB $QEMU_DISP_GL -boot menu=on -m 2048 $QEMU_OVMF
