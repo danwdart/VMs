c2dbe3969521a463f90640475b98bfc5096b155b
Dan Dart <git@dandart.co.uk>
2023-01-28 00:28:28 +0000

    add format=raw to haiku

--- Free/Haiku/r1beta4/install.sh
+++ Free/Haiku/r1beta4/install.sh
@@ -1,1 +1,4 @@
-$QEMU_X86_64 -usb -m 1024 -drive file=haiku.img,if=virtio -drive file=haiku-r1beta4-x86_64-anyboot.iso,if=virtio,media=cdrom -boot d -nic user,model=virtio-net-pci $QEMU_USB $QEMU_DISP_GL $QEMU_OVMF
+$QEMU_X86_64 -device qemu-xhci -m 1024 \
+    -drive file=haiku.img,format=raw,if=virtio \
+    -drive file=haiku-r1beta4-x86_64-anyboot.iso,if=virtio,format=raw,media=cdrom \
+    -boot d -nic user,model=virtio-net-pci $QEMU_BALLOON $QEMU_USB $QEMU_DISP_GL $QEMU_OVMF
