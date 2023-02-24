4affd5b045df354810071857bd9de13aed80c10f
Dan Dart <git@dandart.co.uk>
2023-02-24 21:53:03 +0000

    reactos: use proper raw format

--- Free/ReactOS/x86/latest-gcc-lin-dbg/run.sh
+++ Free/ReactOS/x86/latest-gcc-lin-dbg/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-$QEMU_I386 reactos.img -cdrom reactos*x86*.iso -m 512 -cpu host -smp cores=4,threads=2 $QEMU_USB $QEMU_DISP_STD -monitor stdio
+$QEMU_I386 \
+    -drive file=reactos.img,format=raw \
+    -drive file=reactos-bootcd-0.4.15-dev-5726-g13cf4c5-x86-gcc-lin-dbg.iso,media=cdrom,format=raw \
+    -m 512 -cpu host -smp cores=4,threads=2 $QEMU_USB $QEMU_DISP_STD -monitor stdio
