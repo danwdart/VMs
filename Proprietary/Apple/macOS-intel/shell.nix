b93e077d51e868476cd0187a9d17d12291ee6321
Dan Dart <d.dart@equalsgroup.com>
2023-03-07 17:42:01 +0000

    mac things

--- Proprietary/Apple/macOS-intel/shell.nix
+++ Proprietary/Apple/macOS-intel/shell.nix
@@ -2,11 +2,14 @@ with import <nixpkgs> {};
 runCommand "macOS-intel" {
     buildInputs = [
         dmg2img
-        libguestfs-with-appliance
         python312
-        python312Packages.internetarchive
-        python312Packages.pip
-        python312Packages.click
+        #python312Packages.internetarchive
+        #python312Packages.pip
+        #python312Packages.click
         qemu
-    ];
+    ] ++ (if builtins.currentSystem == "aarch64-darwin" then [
+
+    ] else [
+        libguestfs-with-appliance
+    ]);
 } ""
