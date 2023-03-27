f3a7ba3727ee90422cf0e3d8c9a61d8545e3e92f
Dan Dart <d.dart@equalsmoney.com>
2023-03-27 13:56:19 +0100

    maybe something like this will work for windows

--- Proprietary/Windows/Supported/aarch64/11/run.sh
+++ Proprietary/Windows/Supported/aarch64/11/run.sh
@@ -1,16 +1,19 @@
 qemu-system-aarch64 -M virt,highmem=off \
     -accel hvf \
-    -m 3G -cpu host -serial stdio -smp 4 \
+    -m 3G -cpu cortex-a76 -serial stdio -smp cores=4 \
     -boot menu=on \
     -device intel-hda \
     -device hda-duplex \
     -device ramfb \
     -device qemu-xhci \
     $QEMU_USB \
-    $QEMU_AAVMF \
     -display cocoa,show-cursor=on \
     -drive file=win11.qcow2,if=none,id=hd \
-    -device usb-storage,drive=hd,serial=hd
+    -device nvme,drive=hd,serial=hd \
+    -drive file=pflash0.img,format=raw,if=pflash,readonly=on \
+    -drive file=pflash1.img,format=raw,if=pflash
+
+
+    # $QEMU_AAVMF \
+    # cortex-a55
     # -device virtio-gpu-pci,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT \
-    # -drive file=pflash0.img,format=raw,if=pflash,readonly=on \
-    # -drive file=pflash1.img,format=raw,if=pflash \
