48cccbfaa5e140a01b2f60eccccca6bbbc3b1480
Dan Dart <d.dart@equalsmoney.com>
2023-03-31 18:39:16 +0100

    more nixos updates

--- Free/Linux/x86_64/NixOS/run-macos.sh
+++ Free/Linux/x86_64/NixOS/run-macos.sh
@@ -1,4 +1,4 @@
-$QEMU_X86_64 $QEMU_USB $QEMU_VIRTIO_PERIPH $QEMU_OVMF $QEMU_DISP_GL $QEMU_BALLOON -m 2048 \
+$QEMU_X86_64_MACOS $QEMU_USB $QEMU_VIRTIO_PERIPH $QEMU_OVMF $QEMU_DISP_GL_MACOS $QEMU_BALLOON -m 4096 \
     -drive file=nixos.img,format=raw,if=none,id=hd \
     -drive file=nixos-plasma5-23.05pre468444.4bb072f0a8b-x86_64-linux.iso,format=raw,if=none,id=cd,media=cdrom \
     -device usb-storage,drive=hd \
