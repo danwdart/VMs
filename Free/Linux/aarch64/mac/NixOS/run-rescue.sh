a9c6b00133615058140af7efa15924151295bffa
Dan Dart <d.dart@equalsmoney.com>
2023-08-21 18:14:09 +0100

    add rescue script for nixos

---
+++ Free/Linux/aarch64/mac/NixOS/run-rescue.sh
@@ -0,0 +1,14 @@
+qemu-system-aarch64 -M virt,accel=hvf -m 16G -cpu host -serial stdio -smp cores=8 \
+    -boot menu=on \
+    $QEMU_SND \
+    -display cocoa,show-cursor=on \
+    $QEMU_VIRTIO_PERIPH \
+    $QEMU_BALLOON \
+    $QEMU_DISP_GPU_GL \
+    $QEMU_AAVMF \
+    -drive file=nixos.img,if=none,id=hd \
+    -device virtio-blk-device,drive=hd,serial=hd \
+    -drive file=nixos-broken.img,if=none,id=broken \
+    -device virtio-blk-device,drive=broken,serial=broken \
+    -device virtio-net \
+    -boot menu=on
