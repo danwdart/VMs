bc23483bc42596648ddf8d3d1985119ca2afe3a2
Dan Dart <d.dart@equalsmoney.com>
2023-08-21 18:14:01 +0100

    update nixos

--- Free/Linux/aarch64/mac/NixOS/install.sh
+++ Free/Linux/aarch64/mac/NixOS/install.sh
@@ -6,7 +6,7 @@ qemu-system-aarch64 -M virt,accel=hvf -m 16G -cpu host -serial stdio -smp cores=
     $QEMU_BALLOON \
     -device virtio-gpu-pci,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT \
     $QEMU_AAVMF \
-    -drive file=nixos-plasma5-23.05pre466020.60c1d71f2ba-aarch64-linux.iso,if=none,id=cd \
+    -drive file=nixos-plasma5-23.11pre515819.8ecc900b2f69-aarch64-linux.iso,if=none,id=cd \
     -drive file=nixos.img,if=none,id=hd \
     -device virtio-blk-device,drive=hd \
     -device virtio-blk-device,drive=cd
