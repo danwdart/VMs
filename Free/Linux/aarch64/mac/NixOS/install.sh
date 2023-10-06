86312e48adb547fbb3f53d3cddb49d46a7a189d1
Dan Dart <d.dart@equalsmoney.com>
2023-10-06 18:53:59 +0100

    update NixOS to latest unstable

--- Free/Linux/aarch64/mac/NixOS/install.sh
+++ Free/Linux/aarch64/mac/NixOS/install.sh
@@ -7,7 +7,7 @@ qemu-system-aarch64 -M virt,accel=hvf -m 16G -cpu host -serial stdio -smp cores=
     $QEMU_BALLOON \
     -device virtio-gpu-pci,xres=$SCREEN_WIDTH,yres=$SCREEN_HEIGHT \
     $QEMU_AAVMF \
-    -drive file=nixos-plasma5-23.11pre515819.8ecc900b2f69-aarch64-linux.iso,if=none,id=cd \
+    -drive file=nixos-plasma5-23.11pre532072.81e8f48ebdec-aarch64-linux.iso,if=none,id=cd \
     -drive file=nixos.img,if=none,id=hd \
     -device virtio-blk-device,drive=hd \
     -device virtio-blk-device,drive=cd
