dd2536cb9a9567713380604e50ba19105bff31b7
Dan Dart <d.dart@equalsmoney.com>
2023-12-10 19:07:58 +0000

    update rpios still doesn't go proper accel tho

--- Free/Linux/aarch64/mac/RPiOS/get.sh
+++ Free/Linux/aarch64/mac/RPiOS/get.sh
@@ -1,9 +1,9 @@
 get_installer
-unxz 2023-05-03-raspios-bullseye-arm64.img.xz
-7z x 2023-05-03-raspios-bullseye-arm64.img 0.fat
+unxz 2023-12-05-raspios-bookworm-arm64-full.img.xz
+7z x 2023-12-05-raspios-bookworm-arm64-full.img 0.fat
 mkdir fat
 cd fat
 7z x ../0.fat
 cd ..
 rm 0.fat
-qemu-img resize 2023-05-03-raspios-bullseye-arm64.img 8G
+qemu-img resize 2023-12-05-raspios-bookworm-arm64-full.img 16G
--- Free/Linux/aarch64/mac/RPiOS/install.sh
+++ Free/Linux/aarch64/mac/RPiOS/install.sh
@@ -1,13 +1,12 @@
 # 3b = a53
 # 4b = a72
 qemu-system-aarch64 -M raspi3b -m 1G -cpu host -serial stdio -smp cores=4 \
-    -boot menu=on \
     -display cocoa,show-cursor=on \
     $QEMU_USB2 \
     -device usb-audio \
     -kernel fat/kernel8.img \
     -dtb fat/bcm2710-rpi-3-b.dtb \
-    -append "console=serial0,115200 console=tty1 root=PARTUUID=3e247b30-02 rootfstype=ext4 fsck.repair=yes rootwait quiet init=/usr/lib/raspberrypi-sys-mods/firstboot splash plymouth.ignore-serial-consoles" \
-    -drive file=2023-05-03-raspios-bullseye-arm64.img,if=none,id=hd \
+    -append "console=serial0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 fsck.repair=yes rootwait quiet init=/usr/lib/raspberrypi-sys-mods/firstboot splash plymouth.ignore-serial-consoles" \
+    -drive file=2023-12-05-raspios-bookworm-arm64-full.img,if=none,id=hd \
     -device sd-card,drive=hd \
     -boot menu=on
