975b0ebcdcfcf9b25654705dd977b0715bf39ba5
Dan Dart <d.dart@equalsmoney.com>
2024-01-06 20:42:02 +0000

    back to winworldpc linkx

--- Proprietary/Apple/MacOS/9/9.2/get-mac.sh
+++ Proprietary/Apple/MacOS/9/9.2/get-mac.sh
@@ -1,4 +1,4 @@
-aria2c -s16 -j16 -x16 -k1M https://cloudflare-ipfs.com/ipfs/QmaJGVJYF6D4ZvyeXk9s9Yuk9HLoBzPvzXCqJtiKM6SNuq/Apple%20Mac%20OS%209.2.2%20[eMac].7z
+aria2c -s16 -j16 -x16 -k1M https://winworldpc.com/download/3e2829c2-a318-c39a-11c3-a4e284a2c3a5/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 7z x "Apple Mac OS 9.2.2 [eMac].7z"
 mv "Apple Mac OS 9.2.2 [eMac]/Apple_MacOS_9.2.2_eMac.iso" .
 rm -rf Apple\ Mac\ OS\ 9.2.2\ \[eMac\]*
--- Proprietary/Apple/MacOS/9/9.2/get-macos.sh
+++ Proprietary/Apple/MacOS/9/9.2/get-macos.sh
@@ -1,4 +1,4 @@
-aria2c -s16 -j16 -x16 -k1M -c https://cloudflare-ipfs.com/ipfs/QmckBWgNQ5zufj5KMnKMkzkvKDBxmRBZC67H7NXsCJibKa/Apple%20Mac%20OS%209.2.2%20[PowerMac%20G4].7z
+aria2c -s16 -j16 -x16 -k1M -c https://winworldpc.com/download/3dc39fc2-ac19-18c3-9a11-c3a4e284a2ef/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 7z x "Apple Mac OS 9.2.2 [PowerMac G4].7z"
 rm -rf "Apple Mac OS 9.2.2 [PowerMac G4].7z"
 mv "Apple Mac OS 9.2.2 [PowerMac G4]/Mac OS 9.2.2.iso" .
