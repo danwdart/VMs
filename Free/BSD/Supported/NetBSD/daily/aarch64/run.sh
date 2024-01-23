760abbec649d1beb1172c1d85b053a33752ce1b0
Dan Dart <d.dart@equalsmoney.com>
2024-01-23 18:00:20 +0000

    netbsd daily aarch64

---
+++ Free/BSD/Supported/NetBSD/daily/aarch64/run.sh
@@ -0,0 +1,10 @@
+qemu-system-aarch64 -M virt,accel=hvf,highmem=off -m 3G -cpu cortex-a72 -serial stdio -smp cores=8 \
+    $QEMU_SND \
+    -display cocoa,show-cursor=on \
+    $QEMU_USB \
+    -device ramfb \
+    $QEMU_AAVMF \
+    $QEMU_RNG \
+    -device virtio-net \
+    -drive file=arm64.img,id=hd,if=none \
+    -device usb-storage,drive=hd
