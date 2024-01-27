62bd2eaf9f6851c14f4d9b35107dce3690a44e7e
Dan Dart <d.dart@equalsmoney.com>
2024-01-27 22:49:46 +0000

    freebsd: add rng

--- Free/BSD/Supported/FreeBSD/15.0-current/aarch64/install.sh
+++ Free/BSD/Supported/FreeBSD/15.0-current/aarch64/install.sh
@@ -4,6 +4,7 @@ $QEMU_AARCH64 \
     -device usb-storage,drive=installer \
     -drive file=FreeBSD-15.0-CURRENT-arm64-aarch64-20240104-8bf0882e186e-267378-memstick.img,if=none,id=installer \
     $QEMU_DISP_GPU_GL \
+    $QEMU_RNG \
     -boot menu=on \
     -m 2048 \
     $QEMU_AAVMF
--- Free/BSD/Supported/FreeBSD/15.0-current/aarch64/run.sh
+++ Free/BSD/Supported/FreeBSD/15.0-current/aarch64/run.sh
@@ -2,5 +2,6 @@ $QEMU_AARCH64 \
     freebsd.img \
     $QEMU_USB \
     $QEMU_DISP_GPU_GL \
+    $QEMU_RNG \
     -m 2048 \
     $QEMU_AAVMF
