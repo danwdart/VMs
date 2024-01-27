c5cbb5ea89c30957ddc2554c83d307223fe8c58f
Dan Dart <d.dart@equalsmoney.com>
2024-01-27 22:50:31 +0000

    add a disks script to include... disks

---
+++ Free/Linux/aarch64/mac/NixOS/run-sd-image-disks.sh
@@ -0,0 +1,18 @@
+qemu-system-aarch64 -M virt,accel=hvf -m 16G -cpu host -serial stdio -smp cores=8 \
+    -boot menu=on \
+    $QEMU_SND \
+    $QEMU_VIRTIO_PERIPH \
+    $QEMU_USB \
+    $QEMU_BALLOON \
+    $QEMU_DISP_GPU_GL \
+    $QEMU_AAVMF \
+    -drive file=nixos.img,if=none,id=hd \
+    -device virtio-blk-device,drive=hd \
+    -drive file=sd.img,format=raw,if=none,id=sd \
+    -device virtio-blk-device,drive=sd \
+    -drive file=disk4.img,format=raw,if=none,id=disk4 \
+    -device virtio-blk-device,drive=disk4 \
+    -drive file=disk5.img,format=raw,if=none,id=disk5 \
+    -device virtio-blk-device,drive=disk5 \
+    -nic user,model=virtio-net-pci \
+    -boot menu=on
