4491e0ae1bdd3f90a54e86da3b1a5cf0a7abae3d
Dan Dart <d.dart@equalsmoney.com>
2024-01-29 21:55:20 +0000

    rename disks on macos 8.1

--- Proprietary/Apple/MacOS/8/8.1/create.sh
+++ Proprietary/Apple/MacOS/8/8.1/create.sh
@@ -1,3 +1,3 @@
-create_hd_img 500M 68k.img
-create_hd_img 500M macos.img
+create_hd_img 500M macos-68k.img
+create_hd_img 500M macos-ppc.img
 qemu-img create -f raw pram-macos.img 256b
--- Proprietary/Apple/MacOS/8/8.1/install-ppc.sh
+++ Proprietary/Apple/MacOS/8/8.1/install-ppc.sh
@@ -1,1 +1,1 @@
-qemu-system-ppc -M g3beige -boot d -net nic,model=sungem -net user -drive file=macos.img,media=disk,format=raw,id=hd1 -drive file=MacOS8_1.iso,media=cdrom,id=cd1
+qemu-system-ppc -M g3beige -boot d -net nic,model=sungem -net user -drive file=macos-ppc.img,media=disk,format=raw,id=hd1 -drive file=MacOS8_1.iso,media=cdrom,id=cd1
