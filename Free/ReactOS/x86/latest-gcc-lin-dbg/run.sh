f942356730445d4c123cf8320f1c606a956a8186
Dan Dart <git@dandart.co.uk>
2024-05-07 17:56:06 +0100

    fix reactos scripts

--- Free/ReactOS/x86/latest-gcc-lin-dbg/run.sh
+++ Free/ReactOS/x86/latest-gcc-lin-dbg/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 $QEMU_I386 \
     -drive file=reactos.img,format=raw \
-    -drive file=reactos-bootcd-0.4.15-dev-5726-g13cf4c5-x86-gcc-lin-dbg.iso,media=cdrom,format=raw \
+    -drive file=reactos-bootcd-0.4.15-dev-8006-g3016060-x86-gcc-lin-dbg.iso,media=cdrom,format=raw \
     -m 512 -cpu host -smp cores=4,threads=2 $QEMU_USB $QEMU_DISP_STD -monitor stdio
