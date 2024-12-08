1973a4d3c2b77f37f4a76451d64ca2ebce9947e3
Dan Dart <git@dandart.co.uk>
2024-12-08 23:12:31 +0000

    netbsd stuff

--- Free/BSD/Supported/NetBSD/10.0/x86_64/img/install-usb.sh
+++ Free/BSD/Supported/NetBSD/10.0/x86_64/img/install-usb.sh
@@ -1,6 +1,6 @@
 # https://gnats.netbsd.org/56361
 # NetBSD dies due to a SSE2 bug.
-$QEMU_X86_64_NETBSD -device qemu-xhci $QEMU_USB \
+$QEMU_X86_64_NOSSE2 -device qemu-xhci $QEMU_USB \
     -device usb-storage,drive=hd -drive file=netbsd.img,if=none,id=hd,format=raw \
     -device usb-storage,drive=installer -drive file=NetBSD-10.0-amd64-install.img,if=none,id=installer,format=raw \
     $QEMU_DISP_GL -boot menu=on -m 2G $QEMU_OVMF
--- Free/BSD/Supported/NetBSD/10.0/x86_64/img/run.sh
+++ Free/BSD/Supported/NetBSD/10.0/x86_64/img/run.sh
@@ -1,1 +1,1 @@
-$QEMU_X86_64_NETBSD netbsd.img -device qemu-xhci $QEMU_USB $QEMU_DISP_GL -boot menu=on -m 2G $QEMU_OVMF
+$QEMU_X86_64_NOSSE2 netbsd.img -device qemu-xhci $QEMU_USB $QEMU_DISP_GL -boot menu=on -m 2G $QEMU_OVMF
