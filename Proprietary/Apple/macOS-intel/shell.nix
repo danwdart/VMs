d797db7f3a8a3711c72ded0e83a3464a4fd77c69
Dan Dart <git@dandart.co.uk>
2025-01-04 20:18:51 +0000

    if used use py314

--- Proprietary/Apple/macOS-intel/shell.nix
+++ Proprietary/Apple/macOS-intel/shell.nix
@@ -2,10 +2,10 @@ with import <nixpkgs> {};
 runCommand "macOS-intel" {
     buildInputs = [
         dmg2img
-        python313
-        #python313Packages.internetarchive
-        #python313Packages.pip
-        #python313Packages.click
+        python314
+        #python314Packages.internetarchive
+        #python314Packages.pip
+        #python314Packages.click
         qemu
     ] ++ (if builtins.currentSystem == "aarch64-darwin" then [
 
