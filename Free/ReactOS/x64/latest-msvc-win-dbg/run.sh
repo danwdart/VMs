bd9201d097500be3e4a88725d5a6ce7ffbf870d7
Dan Dart <git@dandart.co.uk>
2025-01-05 21:06:10 +0000

    reactos

--- Free/ReactOS/x64/latest-msvc-win-dbg/run.sh
+++ Free/ReactOS/x64/latest-msvc-win-dbg/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh   
-$QEMU_X86_64 reactos.img -cdrom reactos*x64*.iso -m 512M -cpu host  $QEMU_USB2 -monitor stdio $QEMU_DISP_STD $QEMU_OVMF
+$QEMU_X86_64 reactos.img -cdrom reactos*x64*.iso -m 512M $QEMU_USB2 -monitor stdio $QEMU_DISP_STD $QEMU_OVMF
