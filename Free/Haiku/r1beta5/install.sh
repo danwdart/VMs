fe0761afa20d6bfe567e843afd53a56f48532124
Dan Dart <git@dandart.co.uk>
2025-02-02 22:28:08 +0000

    haiku seems not to support usb disks?

--- Free/Haiku/r1beta5/install.sh
+++ Free/Haiku/r1beta5/install.sh
@@ -1,6 +1,4 @@
 $QEMU_X86_64 $QEMU_USB -m 1G \
-    -drive file=haiku.img,format=raw,id=disk,if=none \
-    -device usb-storage,drive=disk \
-    -drive file=haiku-r1beta5-x86_64-anyboot.iso,format=raw,media=cdrom,id=install,if=none \
-    -device usb-storage,drive=install \
+    haiku.img \
+    -cdrom haiku-r1beta5-x86_64-anyboot.iso \
     -boot d -nic user,model=virtio-net-pci $QEMU_BALLOON $QEMU_DISP_GL $QEMU_OVMF
--- Free/Haiku/r1beta5/run.sh
+++ Free/Haiku/r1beta5/run.sh
@@ -1,4 +1,3 @@
 $QEMU_X86_64 $QEMU_USB -m 1G \
-    -drive file=haiku.img,format=raw,id=disk,if=none \
-    -device usb-storage,drive=disk \
+    haiku.img \
     -boot d -nic user,model=virtio-net-pci $QEMU_BALLOON $QEMU_DISP_GL $QEMU_OVMF
