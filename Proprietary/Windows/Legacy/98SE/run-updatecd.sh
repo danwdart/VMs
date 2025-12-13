3ec3c92a1fa2285b5281004ecdf9ccb053a92b2d
Dan Dart <git@dandart.co.uk>
2025-12-13 19:30:34 +0000

    updatecd for win98?

---
+++ Proprietary/Windows/Legacy/98SE/run-updatecd.sh
@@ -0,0 +1,4 @@
+$QEMU_I386 -M q35,hpet=off -m 512 $QEMU_DISP_CIRRUS -device usb-audio -device qemu-xhci -usb -device usb-tablet -nic user,model=ne2k_pci -monitor stdio \
+    -hda win98se.img \
+    -cdrom WIN98UPD_1-10.ISO \
+    $@
